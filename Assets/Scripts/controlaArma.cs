4a66d09dbfcceee0d8656b021016cdfa73d147c7
ArthurMonteiroPereira <arthurmpereira2010@gmail.com>
2023-12-19 16:38:40 -0300

    power ups, helicopter

--- Assets/Scripts/controlaArma.cs
+++ Assets/Scripts/controlaArma.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 
 public class controlaArma : MonoBehaviour
 {
+    [SerializeField] public float timer_cooldown = 5f;
+    private float timer = 0f;
+    private bool timer_locked_out = false;
     public GameObject bullet;
     public GameObject canoArma;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = timer_cooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if ( timer_locked_out == true )
         {
-            Instantiate(bullet, canoArma.transform.position, transform.rotation);
+            timer -= Time.deltaTime;
+        
+            if ( timer <= 0 )
+            {
+                timer = timer_cooldown;
+                timer_locked_out = false;
+                
+                // a delayed action could be called from here
+                // once the lock-out period expires
+            }
         }
+        if ( timer_locked_out == false )
+        {
+            
+            if (Input.GetButtonDown("Fire1"))
+            {
+                timer_locked_out = true;
+                Instantiate(bullet, canoArma.transform.position, transform.rotation);
+            }
+            
+        }
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PowerUP"){
+            timer_cooldown = timer_cooldown * other.gameObject.GetComponent<powerup>().upgradeCooldownMultiplicator;
+            Destroy(other.gameObject);
+
+        }
+
     }
 }
---
+++ Assets/Scripts/powerup.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerup : MonoBehaviour
+{
+    public float upgradeCooldownMultiplicator = 0.90f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+}
